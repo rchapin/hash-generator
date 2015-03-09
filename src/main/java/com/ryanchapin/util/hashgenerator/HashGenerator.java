4b9c5388359d6135f5989433dcb23549d9aa8074
Ryan Chapin <rchapin@users.noreply.github.com>
2015-03-09 08:05:56 -0400

    Interim, mid development commit

---
+++ src/main/java/com/ryanchapin/util/hashgenerator/HashGenerator.java
@@ -0,0 +1,231 @@
+package com.ryanchapin.util.hashgenerator;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * The <code>Hash-Generator</code> is a class used for generating hashes for
+ * various types of input data.
+ * <p>
+ * It supports any of the hash algorithms that are supported by the
+ * {@link java.security.MessageDigest} class.
+ * <p>
+ * The class is thread safe <b>depending on how it is instantiated and/or
+ * called</b>.  Used in the following manner it is thread safe:
+ * <p><blockquote><pre>
+ *    HashGenerator hashGenerator = new HashGenerator();
+ *    String sha1Hash = hashGenerator.createHash("This is a test", "SHA-1"); 
+ * </pre></blockquote>
+ * 
+ * 
+  
+ * 
+ * @author Ryan Chapin
+ * @since  2015-03-01
+ *
+ */
+public class HashGenerator {
+   
+   /**
+    * Hash algorithm to be used to generate hashes for subsequent calls to
+    * any of the overloaded <code>createHash</code> methods. 
+    */
+   private String hashAlgo;
+   
+   private ByteArrayOutputStream bos;
+   
+   private DataOutputStream dos;
+   
+   private MessageDigest md;
+   
+   // -------------------------------------------------------------------------
+   // Accessor/Mutators:
+   //
+   
+   
+   // -------------------------------------------------------------------------
+   // Constructor:
+   //
+   
+   /**
+    * Initializes a new {@code HashGenerator} instance such that subsequent
+    * calls can be made passing in only the data to be hashed.
+    * <p>
+    * In this context the instance is <b>NOT</b> thread safe and thread safety
+    * should be managed by the client.  This constructor should be used in
+    * situations where client code is making large number of calls to this
+    * class and there are issues arising from garbage collection of objects
+    * that are instantiated on the stack.
+    * <p>
+    * In that case, the developer may opt to instantiate a new instance per
+    * thread, or simply synchronize calls to the class if that will not
+    * cause a performance bottleneck.
+    * 
+    * @param hashAlgo
+    *        Hash algorithm to be used to create hashes.
+    */
+   public HashGenerator(String hashAlgo) {
+      this.hashAlgo = hashAlgo;
+   }
+   
+   
+   // -------------------------------------------------------------------------
+   // Member Methods:
+   //
+   
+   private void checkHashAlgoField() throws IllegalStateException {
+      if (null == this.hashAlgo || this.hashAlgo.isEmpty()) {
+         throw new IllegalStateException("No hashing algorithm was set for this HashGenerator instance.");
+      }  
+   }
+   
+   private static void checkHashAlgoField(String hashAlgo) throws IllegalArgumentException {
+      if (null == hashAlgo || hashAlgo.isEmpty()) {
+         throw new IllegalStateException("No hashing algorithm was provided.");
+      }
+   }
+   
+   private static void checkStringEncoding(String encoding) {
+      
+   }
+   
+   public String createHash(String input, String encoding) {
+      checkHashAlgoField();
+      
+      // Generate a byte array from the input String.
+      byte[] inByteArray = null;
+      try {
+         inByteArray = input.getBytes(encoding);
+      } catch (UnsupportedEncodingException e) {
+         e.printStackTrace();
+      }
+      return bytesToHex(computeHashBytes(inByteArray));
+   }
+   
+   public static String createHash(String input, String encoding, String hashAlgorithm) {
+      checkHashAlgoField(hashAlgorithm);
+      
+      // Generate a byte array from the input String.
+      byte[] inByteArray = null;
+      try {
+         inByteArray = input.getBytes(encoding);
+      } catch (UnsupportedEncodingException e) {
+         e.printStackTrace();
+      }
+      return bytesToHex(computeHashBytes(inByteArray, hashAlgorithm));
+   }
+   
+   /**
+    * Computes the hashed bytes for the byte array representation of the input
+    * data.  To be used when thread safety is handled by the client code.
+    * 
+    * @param inputBytes
+    *        byte array representing the data to be hashed.
+    *        
+    * @return the array of bytes representing the hashed data.
+    */
+   public byte[] computeHashBytes(byte[] inputBytes) {
+      if (null == md) {
+         try {
+            md = MessageDigest.getInstance(hashAlgo);
+         } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+         }
+      }
+      md.reset();
+      md.update(inputBytes);
+      return md.digest();
+   }
+   
+   /**
+    * 
+    * @param inputBytes
+    * @param hashAlgorithm
+    * @return
+    */
+   public static byte[] computeHashBytes(byte[] inputBytes, String hashAlgorithm) {
+      // Instantiate a MessageDigest instance configured with the desired
+      // algorithm.
+      MessageDigest msgDigest = null;
+      try {
+         msgDigest = MessageDigest.getInstance(hashAlgorithm);
+      } catch(NoSuchAlgorithmException e) {
+         e.printStackTrace();
+      }
+      return msgDigest.digest(inputBytes);
+   }
+   
+   /**
+    * 
+    * @param hashBytes
+    * @return
+    */
+   public static String bytesToHex(byte[] hashBytes) {
+      
+      // Convert the hashBytes to a String of hex values
+      StringBuilder retVal   = new StringBuilder();
+      StringBuilder hexValue = new StringBuilder();
+      
+      for (byte hashByte : hashBytes) {
+         // Flush our StringBuilder to be used as a container for the
+         // hex value for each byte as it is read.
+         hexValue.delete(0, hexValue.length());
+         hexValue.append(Integer.toHexString(0xFF & hashByte));
+         
+         // Add a trailing '0' if our hexValue is only 1 char long
+         if (hexValue.length() == 1) {
+            hexValue.insert(0, '0');
+         }
+         retVal.append(hexValue);
+      }
+      return retVal.toString();
+   }
+   
+   /*
+   public String createHash(long input, String hashAlgorithm) {
+      // Generate a byte array from the long
+      // Extract the byte value of the long
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        try {
+            dos.writeLong(input);
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+        try {
+            dos.flush();
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+
+        byte[] byteArray = bos.toByteArray();
+        return bytesToHex(computeHashBytes(byteArray, hashAlgorithm));
+   }
+   
+
+   
+   private static byte[] computeHashBytes(byte[] inputBytes, String hashAlgorithm) {
+      // Instantiate a MessageDigest instance configured with the desired
+      // algorithm.
+      MessageDigest md = null;
+      try {
+         md = MessageDigest.getInstance(hashAlgorithm);
+      } catch(NoSuchAlgorithmException e) {
+         e.printStackTrace();
+      }
+      
+      // This isn't necessary in this context, but should this
+      // be refactored to use the MessageDigest as a member this
+      // enables the reuse of the same MessageDigest instance.
+      md.reset();
+      md.update(inputBytes);
+      return md.digest();
+   }
+   
+   
+   */
+}
---
+++ src/test/java/com/ryanchapin/util/hashgenerator/HashGeneratorTest.java
@@ -0,0 +1,102 @@
+package com.ryanchapin.util.hashgenerator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HashGeneratorTest {
+   
+   private static final Logger LOGGER = LoggerFactory.getLogger(HashGeneratorTest.class);
+   
+   @Rule
+   public TestName testName = new TestName();
+   
+   private static final String INPUT_STRING_A = "This is a test string";
+   private static final String INPUT_STRING_B = "GeNvUM^$$j,gMPO@7s0O@";
+   
+   /** Array of all of the currently supported hash algorithms */
+   private static final String[] HASH_ALGOS = {"MD2", "MD5", "SHA-1", "SHA-256", "SHA-384", "SHA-512"};
+   
+   // Need to test each of the methods for generating a hash
+   // Do I mock the MessageDigest class or test it as well?
+   // For it to be a true unit test, I should mock that class and not
+   // rely on it
+   // Or is it integral, and do I just test that hashes against know input
+   // and what should be output?
+   //
+   // I think that is the way to go, and to test all of the error
+   // conditions that should be handled.
+   
+
+   
+   @Test
+   public void shouldCorrectlyConvertBytesToHex() {
+      LOGGER.info("Running test: {}", testName.getMethodName());
+      
+      String expectedOutput00 = "010203180fab9b80";
+      byte[] input00 = new byte[] {
+            (byte)0b00000001, // 01
+            (byte)0b00000010, // 02
+            (byte)0b00000011, // 03
+            (byte)0b00011000, // 18
+            (byte)0b00001111, // 0f
+            (byte)0b10101011, // ab
+            (byte)0b10011011, // 9b
+            (byte)0b10000000  // 80
+      };
+      String hexVal00 = HashGenerator.bytesToHex(input00);
+      LOGGER.debug("test = {}", hexVal00);
+      assertEquals("hexVal01 was not the correct hex value",
+            expectedOutput00, hexVal00);
+      
+      String expectedOutput01 = "00fffefdfcfbfaf9f8f7f6f5f4f3f2f1f000";
+      byte[] input01 = new byte[] {
+            (byte)0b00000000, // 00
+            (byte)0b11111111, // ff
+            (byte)0b11111110, // fe
+            (byte)0b11111101, // fd
+            (byte)0b11111100, // fc
+            (byte)0b11111011, // fb
+            (byte)0b11111010, // fa
+            (byte)0b11111001, // f9
+            (byte)0b11111000, // f8
+            (byte)0b11110111, // f7
+            (byte)0b11110110, // f6
+            (byte)0b11110101, // f5
+            (byte)0b11110100, // f4
+            (byte)0b11110011, // f3
+            (byte)0b11110010, // f2
+            (byte)0b11110001, // f1
+            (byte)0b11110000, // f0
+            (byte)0b00000000  // 00
+      };
+      String hexVal01 = HashGenerator.bytesToHex(input01);
+      LOGGER.debug("test = {}", hexVal01);
+      assertEquals("hexVal01 was not the correct hex value",
+            expectedOutput01, hexVal01);
+   }
+   
+   @Test
+   public void shouldCorrectlyHashStringInputUTF8() {
+      LOGGER.info("Running test: {}", testName.getMethodName());
+      
+      String hashUTF8 = HashGenerator.createHash(INPUT_STRING_A, "UTF-8", "MD5");
+      LOGGER.debug("hashUTF8    = {}", hashUTF8);
+      
+      String hashUSASCII = HashGenerator.createHash(INPUT_STRING_A, "US-ASCII", "MD5");
+      LOGGER.debug("hashUSASCII = {}", hashUSASCII);
+      
+      String hashUTF16LE = HashGenerator.createHash(INPUT_STRING_A, "UTF-16LE", "MD5");
+      LOGGER.debug("hashUTF16LE = {}", hashUTF16LE);
+      
+   }
+   
+   // 1256987432 = 1001010111011000001101100101000
+   
+   // 9223372036854775807L = 0x7FFFFFFFFFFFFFFF
+   
+}
